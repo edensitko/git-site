5b88c5c095acfe0a6cf7bcfeefc51087911d0438
edensitko <edensitko@users.noreply.github.com>
2026-03-02 19:27:47 +0000

    deploy: 10bdeb63a28a6d0f21df0c0e87111ebb7616fad8

--- _next/static/chunks/app/[username]/not-found-80da40574c5190bb.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[491],{8805:function(n,e,u){Promise.resolve().then(u.t.bind(u,2972,23))}},function(n){n.O(0,[972,971,117,744],function(){return n(n.s=8805)}),_N_E=n.O()}]);
